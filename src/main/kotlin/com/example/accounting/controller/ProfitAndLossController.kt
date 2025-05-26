9ec39af4bbbbfe4778f565063c4d566ce3b9782a
木下匠 <takumin4260@gmail.com>
2025-05-26 13:40:29 +0900

    Add request params in getPl

--- src/main/kotlin/com/example/accounting/controller/ProfitAndLossController.kt
+++ src/main/kotlin/com/example/accounting/controller/ProfitAndLossController.kt
@@ -5,14 +5,19 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import java.time.YearMonth
 
 @Controller
 class ProfitAndLossController(
     private val listProfitAndLossUseCase: ListProfitAndLossUseCase
 ) {
     @GetMapping("/pl")
-    fun list(model: Model): String {
-        val pl = listProfitAndLossUseCase.execute()
+    fun list(
+        model: Model,
+        @RequestParam month: YearMonth = YearMonth.now()
+    ): String {
+        val pl = listProfitAndLossUseCase.execute(month)
         model["pl"] = pl
         return "pl"
     }
--- src/main/kotlin/com/example/accounting/usecase/journal/ListProfitAndLossUseCase.kt
+++ src/main/kotlin/com/example/accounting/usecase/journal/ListProfitAndLossUseCase.kt
@@ -4,21 +4,24 @@ import com.example.accounting.domain.account.AccountRepository
 import com.example.accounting.domain.account.AccountType
 import com.example.accounting.domain.common.DateRange
 import com.example.accounting.domain.journal.JournalRepository
+import com.example.accounting.domain.profit_and_loss.Pl
 import com.example.accounting.domain.profit_and_loss.ProfitAndLoss
+import org.springframework.cglib.core.Local
 import org.springframework.stereotype.Service
 import java.time.LocalDate
+import java.time.YearMonth
 
 @Service
 class ListProfitAndLossUseCase(
     private val accountRepository: AccountRepository,
     private val journalRepository: JournalRepository,
 ) {
-    fun execute(): Any {
-        // 2024年の1月
-        val today = LocalDate.of(2024,1,10)
+    fun execute(
+        month: YearMonth,
+    ): Pl {
         val range = DateRange.of(
-            today.withDayOfMonth(1),
-            today.withDayOfMonth(today.lengthOfMonth())
+            month.atDay(1),
+            month.atEndOfMonth(),
         )
 
         /* P/L に載せる科目(収益・費用)を取得 */
