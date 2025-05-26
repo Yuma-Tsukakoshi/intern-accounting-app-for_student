1537034f88ad55a4698b6f748ae0149f3be971bb
木下匠 <takumin4260@gmail.com>
2025-05-26 18:49:26 +0900

    Fix controller and view

--- src/main/kotlin/com/example/accounting/usecase/profit_and_loss/ListProfitAndLossUseCase.kt
+++ src/main/kotlin/com/example/accounting/usecase/profit_and_loss/ListProfitAndLossUseCase.kt
@@ -4,6 +4,7 @@ import com.example.accounting.domain.account.AccountRepository
 import com.example.accounting.domain.account.AccountType
 import com.example.accounting.domain.common.DateRange
 import com.example.accounting.domain.journal.JournalRepository
+import com.example.accounting.domain.profit_and_loss.JournalPair
 import com.example.accounting.domain.profit_and_loss.Pl
 import com.example.accounting.domain.profit_and_loss.ProfitAndLoss
 import org.springframework.stereotype.Service
@@ -15,22 +16,38 @@ class ListProfitAndLossUseCase(
     private val journalRepository: JournalRepository,
 ) {
     fun execute(
-        month: YearMonth,
+        lastMonth: YearMonth?,
+        currentMonth: YearMonth,
     ): Pl {
-        val range = DateRange.Companion.of(
-            month.atDay(1),
-            month.atEndOfMonth(),
+        val lastMonthRange = lastMonth?.let { lastMonth ->
+            DateRange.Companion.of(
+                lastMonth.atDay(1),
+                lastMonth.atEndOfMonth(),
+            )
+        }
+
+        val currentMonthRange = DateRange.Companion.of(
+            currentMonth.atDay(1),
+            currentMonth.atEndOfMonth(),
         )
 
         /* P/L に載せる科目(収益・費用)を取得 */
         val plTypes = AccountType.Companion.getListForPL()
         val accounts = accountRepository.filterByAccountTypes(plTypes)
         /* 今月の仕訳を取得 */
-        val journals = journalRepository.filterByDateRange(range)
+        val lastJournals = lastMonthRange?.let { lastMonthRange ->
+            journalRepository.filterByDateRange(lastMonthRange)
+        }
+        val currentJournals = journalRepository.filterByDateRange(currentMonthRange)
+
+        val journalPair = JournalPair(
+            last = lastJournals,
+            current = currentJournals,
+        )
 
         val result =  ProfitAndLoss.Companion.create(
             accounts,
-            journals
+            journalPair
         )
         return result
     }
